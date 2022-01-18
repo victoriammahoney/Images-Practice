9ca6548a70237a8f54c4186c82c33f02eb738ca6
Victoria Mahoney <63237@psdschools.org>
2022-01-18 13:46:16 -0700

    Finished Product

--- Images Practice/ViewController.swift
+++ Images Practice/ViewController.swift
@@ -9,11 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var appleView: UIImageView!
+    
+    @IBOutlet weak var orangeView: UIImageView!
+    
+    @IBOutlet weak var grapeView: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        orangeView.isHidden = true
+        appleView.isHidden = true
+        grapeView.isHidden = true
+ 
+        
     }
 
-
+    @IBAction func appleButton(_ sender: Any) {
+        
+        orangeView.isHidden = true
+        grapeView.isHidden = true
+        appleView.isHidden = false
+    }
+    
+    @IBAction func orangeButton(_ sender: Any) {
+        
+        orangeView.isHidden = false
+        grapeView.isHidden = true
+        appleView.isHidden = true
+    }
+    
+    @IBAction func grapeButton(_ sender: Any) {
+        
+        orangeView.isHidden = true
+        appleView.isHidden = true
+        grapeView.isHidden = false
+        
+    }
+    
 }
 
